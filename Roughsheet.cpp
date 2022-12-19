b3455c10c083a85326bca1937d9a253c54126602
Padmapiyush <padmpiyush2801@gmail.com>
2022-12-19 21:15:07 +0530

    minute

--- Roughsheet.cpp
+++ Roughsheet.cpp
@@ -1,13 +1,8 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
 int main(){
-    int n=0;
-    while(n<10){
-        if(n==4){
-            continue;
-        }
-        cout<<n<<endl;
-        n++;
-    }
+    
+    
 }
