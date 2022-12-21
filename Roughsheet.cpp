709568e4fb815a55ff185560539433cd71c4637c
Padmapiyush <padmpiyush2801@gmail.com>
2022-12-22 02:01:12 +0530

    minute

--- Roughsheet.cpp
+++ Roughsheet.cpp
@@ -1,9 +1,13 @@
 #include<iostream>
-#include<vector>
 using namespace std;
 
 int main(){
-    
-
-    
+    int scores[5], sum = 0;
+    for(int i=0;i<5;i++){
+        cin>>scores[i];
+    }
+    for(int i=0s;i<5;i++){
+        cout<<scores[i]<<endl;
+    }
+    return 0;
 }
