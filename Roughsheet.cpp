e964e4c944d696e52bbe76d742487cbaed3ab01b
Padmapiyush <padmpiyush2801@gmail.com>
2023-01-11 17:47:47 +0530

    started

--- Roughsheet.cpp
+++ Roughsheet.cpp
@@ -1,13 +1,19 @@
-#include<iostream>
-using namespace std;
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
-int main(){
-    int scores[5], sum = 0;
-    for(int i=0;i<5;i++){
-        cin>>scores[i];
-    }
-    for(int i=0s;i<5;i++){
-        cout<<scores[i]<<endl;
-    }
+int main() 
+{
+
+    int n;
+    scanf("%d", &n);
+  	for(int i=1;i<=2*n-1;i++){
+          for(int j=1;j<=2*n-1;j++){
+              printf("%d ",n);
+          }
+          printf("\n");
+          n--;
+      }
     return 0;
 }
--- roughsheet2.c
+++ roughsheet2.c
@@ -1,7 +1,18 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
-int main(){
+int main() 
+{
 
-    
+    int n;
+    scanf("%d", &n);
+  	for(int i=1;i<=2*n-1;i++){
+          for(int j=1;j<=2*n-1;j++){
+              printf("%d ",n);
+          }
+          printf("\n");
+      }
+    return 0;
 }
