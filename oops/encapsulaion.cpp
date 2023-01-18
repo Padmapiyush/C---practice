4cdef1a5c0687973ed2881b0d796c1f29521e23b
Padmapiyush <padmpiyush2801@gmail.com>
2023-01-18 23:20:40 +0530

    added encapsulation file

---
+++ oops/encapsulaion.cpp
@@ -0,0 +1,4 @@
+/*
+Encapsulation in C++ is defined as the wrapping up of data and information in a single unit.
+In Object Oriented Programming, Encapsulation is defined as binding together the data and the functions that manipulate them.
+*/
