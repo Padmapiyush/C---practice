fa9b4d235a312fc2253e5f6eef6fe63f969c5457
Padmapiyush <padmpiyush2801@gmail.com>
2023-03-22 16:47:38 +0530

    basic changes

--- Roughsheet.cpp
+++ Roughsheet.cpp
@@ -1,19 +1,22 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
 
-int main() 
-{
 
-    int n;
-    scanf("%d", &n);
-  	for(int i=1;i<=2*n-1;i++){
-          for(int j=1;j<=2*n-1;j++){
-              printf("%d ",n);
-          }
-          printf("\n");
-          n--;
-      }
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    int n,ele;
+    cin>>n;
+    vector<int>v;
+    for(int i=1;i<=n;i++){
+        cin>>ele;
+        v.push_back(ele);
+    } 
+    for(int i=1;i<=n;i++){
+        cout<<v[i]<<" ";
+    }
     return 0;
 }
